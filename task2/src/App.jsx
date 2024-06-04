aa37f50f6f5ee44914a785c2109c54cd261a223b
Akshat Bisht <bishtakshat64@gmail.com>
2024-06-04 18:50:34 +0530

    app completed with artist

--- task2/src/App.jsx
+++ task2/src/App.jsx
@@ -1,11 +1,13 @@
+import Artist from "./components/Artist";
 import Nav from "./components/Nav";
+
 
 
 
 function App() {
   return (
-    <div className="absolute top-0 left-0 w-[1600px] h-[1800px] bg-gray-100 opacity-100" 
-    style={{ backgroundPosition: '0% 0%', backgroundRepeat: 'no-repeat', backgroundOrigin: 'padding-box' ,overflow:"hidden"}}
+    <div className="absolute top-0 left-0 w-[1600px] h-[1800px] bg-[#F5F5F5] opacity-100" 
+    style={{ backgroundPosition: '0% 0%', backgroundRepeat: 'no-repeat', backgroundOrigin: 'padding-box'}}
     >
       <div className="">
         <div className="grid grid-cols-3 gap-4 h-full">
@@ -15,7 +17,9 @@ function App() {
             
           </div>
           <div className="border-2 border-red-500 h-full">Grid 2</div>
-          <div className="border-2 border-red-500 h-full">Grid 3</div>
+          <div className="h-full">
+           <Artist/>
+          </div>
         </div>
       </div>
     </div>
